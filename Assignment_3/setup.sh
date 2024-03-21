1ac39305cc9294f9e791ce6d2596e12fdfd1a8be
EmilieAndreasen <emiliemandreasen@outlook.dk>
2024-03-21 13:24:17 +0100

    Assignment 3 made, no README.md yet

--- Assignment_3/setup.sh
+++ Assignment_3/setup.sh
@@ -6,6 +6,6 @@ python -m venv env
 source ./env/bin/activate
 # install requirements
 pip install --upgrade pip
-pip install -r ../requirements.txt
+pip install -r requirements.txt
 # close the environment
 deactivate

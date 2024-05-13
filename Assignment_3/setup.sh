9890600741d45bb9da8428c0b09f3117c5381281
EmilieAndreasen <emiliemandreasen@outlook.dk>
2024-05-13 22:48:55 +0200

    Saving before maintenance on UCloud 14/05

--- Assignment_3/setup.sh
+++ Assignment_3/setup.sh
@@ -7,5 +7,8 @@ source ./env/bin/activate
 # install requirements
 pip install --upgrade pip
 pip install -r requirements.txt
+
+#pip install scipy==1.11.0 #if the code suddenly doesn't work apply this (note to self)
+
 # close the environment
 deactivate
